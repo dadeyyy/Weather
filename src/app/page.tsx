25beb509fac47d906edfddc2e463c5bf557ddcdb
andrei <andrei.lazo.30@gmail.com>
2024-06-09 15:49:48 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,31 @@ import RainfallIntensity from '@/components/rainfall/intensity';
 import RainfallWarning from '@/components/rainfall/warning';
 import Rainfallmm from '@/components/rainfall/rainfallmm';
 import RecentData from '@/components/rainfall/recent';
+import { link } from 'fs';
 
 export default function Home() {
   return (
+    
     <div
-      className="bg-cover bg-center h-100 sm:h-screen flex justify-center items-center"
+      className="bg-cover bg-center h-screen flex justify-center items-center"
       style={{
         backgroundImage: `url(/weather.webp)`,
       }}
     >
-      <main className="container mx-auto backdrop-blur-md bg-slate-800 rounded-2xl opacity-80 h-full sm:h-4/5 text-white  flex flex-col sm:flex-row p-9 gap-10">
+      <link
+        rel="icon"
+        href="/wallpaper-cloudy.png"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+      <main className="container mx-auto backdrop-blur-md bg-slate-800 rounded-2xl opacity-80  text-white flex flex-col sm:flex-row p-9 gap-10 sm:h-auto h-full justify-center items-center max-w-4xl">
         <RecentData/>
 
-        <div className=" w:full sm:w-3/5 flex flex-col gap-5">
+        {/* <div className=" w:full sm:w-3/5 flex flex-col gap-5">
           <Rainfallmm />
           <RainfallIntensity />
           <RainfallWarning/>
-        </div>
+        </div> */}
       </main>
     </div>
   );
--- src/components/rainfall/recent.tsx
+++ src/components/rainfall/recent.tsx
@@ -4,75 +4,86 @@ import { RecentDataType } from '@/lib/types';
 
 import { checkIntensityText, formatDate, formatDateRecent } from '@/lib/utils';
 
-async function getRecentData(){
-    const res = await fetch('https://api.thingspeak.com/channels/2531448/feeds/last.json?api_key=UFR2I5V9Z9KMQ10X')
+async function getRecentData() {
+  const res = await fetch(
+    'https://api.thingspeak.com/channels/2531448/feeds/last.json?api_key=UFR2I5V9Z9KMQ10X'
+  );
 
-    if(!res.ok){
-        throw new Error('Failed to fetch recent data')
-    }
+  if (!res.ok) {
+    throw new Error('Failed to fetch recent data');
+  }
 
-    return res.json()
+  return res.json();
 }
 
 const RecentData = async () => {
-    const data = await getRecentData() as RecentDataType;
-   
-    const warning = checkIntensityText(data.field3)
-    const formattedDateRecent = formatDateRecent(new Date(data.created_at))
+  const data = (await getRecentData()) as RecentDataType;
+
+  const warning = checkIntensityText(data.field3);
+  const formattedDateRecent = formatDateRecent(new Date(data.created_at));
   return (
-    <div className="flex flex-col w-full sm:w-2/5">
-          <div className="flex flex-col sm:flex-row gap-4  w-full">
-            <SoilMoistureBtn/>  
-            
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col sm:flex-row gap-4 w-full">
+        <SoilMoistureBtn />
+      </div>
+      <div className="flex flex-col items-center h-full p-8  bg-slate-900 rounded-xl mt-2">
+        <div className=" flex justify-center items-center gap-5 mb-5 p-10">
+          <div className='flex flex-col justify-center items-start'>
+            <p className="sm:text-7xl font-semibold text-5xl">
+              {formattedDateRecent.time}
+            </p>
+            <p className="sm:text-3xl text-2xl">{formattedDateRecent.date}</p>
           </div>
-          <div className="flex flex-col items-center h-full p-8  bg-slate-900 rounded-xl mt-2">
-            <div className="h-1/2 flex flex-col justify-center items-center gap-5 mb-5">
-              <p className={`text-4xl sm:text-6xl text-center ${warning?.color}`}>{warning?.rainfallWarning}</p>
-              <p className="sm:text-3xl text-2xl">{formattedDateRecent.date}</p>
-              <p className="sm:text-7xl text-5xl">{formattedDateRecent.time}</p>
-              
+
+          <p className={`text-4xl sm:text-6xl text-center ${warning?.color}`}>
+            {warning?.rainfallWarning}
+          </p>
+        </div>
+        <div className=" flex flex-col justify-center w-full  h-1/2 gap-6">
+          <div className="flex flex-col sm:flex-row gap-6">
+            <div className="w-full flex flex-col justify-center   rounded-lg p-2 bg-sky-400/90 gap-2">
+              <div className="flex opacity-50 gap-1">
+                <Image
+                  src={`thermometer.svg`}
+                  width={25}
+                  height={25}
+                  alt="Thermometer"
+                />
+
+                <h1 className="text-black  font-extrabold">RAINFALL IN MM</h1>
+              </div>
+              <span className="text-4xl">{data.field2}</span>
+              <span className="opacity-70 flex gap-5">
+                <p>{formattedDateRecent.date}</p>
+                <p>{formattedDateRecent.time}</p>
+              </span>
             </div>
-            <div className=" flex flex-col justify-center w-full  h-1/2 gap-6">
-              <div className="flex flex-col sm:flex-row gap-6">
-                <div className="w-full flex flex-col justify-center   rounded-lg p-2 bg-sky-400/90 gap-2">
-                  <div className="flex opacity-50 gap-1">
-                    <Image
-                      src={`thermometer.svg`}
-                      width={25}
-                      height={25}
-                      alt="Thermometer"
-                    />
+          </div>
+          <div className="flex flex-col sm:flex-row gap-6">
+            <div className="w-full flex flex-col justify-center   rounded-lg p-2 bg-sky-400/90 gap-2">
+              <div className="flex opacity-50 gap-1">
+                <Image
+                  src={`droplet.svg`}
+                  width={25}
+                  height={25}
+                  alt="Thermometer"
+                />
 
-                    <h1 className="text-black  font-extrabold">RAINFALL IN MM</h1>
-                  </div>
-                  <span className="text-4xl">{data.field2}</span>
-                  <span className="opacity-70">
-                    {formatDate(new Date(data.created_at))}
-                  </span>
-                </div>
+                <h1 className="text-black  font-extrabold">
+                  RAINFALL INTENSITY
+                </h1>
               </div>
-              <div className="flex flex-col sm:flex-row gap-6">
-                <div className="w-full flex flex-col justify-center   rounded-lg p-2 bg-sky-400/90 gap-2">
-                  <div className="flex opacity-50 gap-1">
-                    <Image
-                      src={`droplet.svg`}
-                      width={25}
-                      height={25}
-                      alt="Thermometer"
-                    />
-
-                    <h1 className="text-black  font-extrabold">RAINFALL INTENSITY</h1>
-                  </div>
-                  <span className="text-4xl">{data.field3}</span>
-                  <span className="opacity-70">
-                  {formatDate(new Date(data.created_at))}
-                  </span>
-                </div>
-              </div>
+              <span className="text-4xl">{data.field3}</span>
+              <span className="opacity-70 flex gap-5">
+                <p>{formattedDateRecent.date}</p>
+                <p>{formattedDateRecent.time}</p>
+              </span>
             </div>
           </div>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RecentData
+export default RecentData;
